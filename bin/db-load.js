8773a7cd4fb1d4afff10b86b04364ccfb663c32b
Raymond Neilson <rneilson@interloper.ca>
2016-08-24 22:46:01 -0400

    Revamped db-load.js to use sync but make each file's content a single txn

--- bin/db-load.js
+++ bin/db-load.js
@@ -18,7 +18,7 @@ const dbpath = path.isAbsolute(appcfg.dbpath) ? appcfg.dbpath : path.resolve(__d
 const dbw = new DBWriter({
 	path: dbpath,
 	user_id_str: appcfg.user.id_str,
-	noSync: true
+	// noSync: true
 });
 
 // Get file list
@@ -29,20 +29,93 @@ for (let fname of filenames) {
 	loadfile(fname);
 }
 
-// Sync and close
-dbw.sync(err => {
-	if (err) {
-		console.error(err);
-	}
-	dbw.close();
-});
+// // Sync and close
+// dbw.sync(err => {
+// 	if (err) {
+// 		console.error(err);
+// 	}
+// 	dbw.close();
+// });
 
 console.log('Finished!');
 
 
 // Funcs
 function loadfile (name) {
+	const types = ['user', 'user_tweet', 'other_tweet', 'delete', 'favorite', 'unfavorite'];
+	let count = {
+		'user': 0,
+		'user_tweet': 0,
+		'other_tweet': 0,
+		'delete': 0,
+		'favorite': 0,
+		'unfavorite': 0
+	};
 	let filename = path.resolve(name);
+	let ret = false;
+	let total = 0;
+
+	function writefn (type, data, txn) {
+		let retw = false;
+
+		switch (type) {
+			case 'queue':
+				retw = writeq(data);
+				break;
+			case 'user':
+				retw = dbw.writeuser(data, txn);
+				break;
+			case 'user_tweet':
+				retw = dbw.writetweet(data, txn);
+				break;
+			case 'other_tweet':
+				retw = dbw.writetweet(data, txn);
+				break;
+			case 'delete':
+				retw = dbw.deletetweet(data, txn);
+				break;
+			case 'favorite':
+				retw = dbw.writefav(data, txn);
+				break;
+			case 'unfavorite':
+				retw = dbw.deletefav(data, txn);
+				break;
+		}
+
+		return retw;
+	}
+
+	function writeq (queue) {
+		const types = ['user', 'user_tweet', 'other_tweet', 'delete', 'favorite', 'unfavorite'];
+		let retq = false;
+
+		// Open transaction
+		let qtxn = dbw.begin();
+
+		try {
+			// Recursively call
+			for (let i = 0; i < queue.length; i++) {
+				let item = queue[i];
+				if (_.has(item, 'type') && _.has(item, 'data')) {
+					// Leave return value to end of queue, but return true if any write succeeds
+					if (writefn(item.type, item.data, qtxn)) {
+						retq = true;
+						if (_.has(count, item.type)) {
+							count[item.type]++;
+						}
+					}
+				}
+			}
+			qtxn.commit();
+		}
+		catch (e) {
+			qtxn.abort();
+			console.error(e);
+			retq = false;
+		}
+
+		return retq;
+	}
 
 	try {
 		// Read file into memory
@@ -53,33 +126,25 @@ function loadfile (name) {
 
 		if (eventlist.length > 0) {
 			console.log(`Processing ${filename}, ${eventlist.length} items`);
-			
-			// Check/write relevant events
-			// Index events in a dumpstream aren't needed
-			for (let event of eventlist) {
-				switch (event.type) {
-					case 'user':
-						dbw.writeuser(event.data);
-						break;
-					case 'user_tweet':
-						dbw.writetweet(event.data);
-						break;
-					case 'other_tweet':
-						dbw.writetweet(event.data);
-						break;
-					case 'delete':
-						dbw.deletetweet(event.data);
-						break;
-					case 'favorite':
-						dbw.writefav(event.data);
-						break;
-					case 'unfavorite':
-						dbw.deletefav(event.data);
-						break;
+			if (writeq(eventlist)) {
+				let counts = [];
+				for (let name of types) {
+					let num = count[name];
+					if (num > 0) {
+						total += num;
+						counts.push(`${num} ${name}${num > 1 ? 's' : ''}`);
+					}
 				}
+				console.log(`Processed ${eventlist.length} items, updated ${total} (${counts.join(', ')})`);
 			}
+			else {
+				console.log(`Processed ${eventlist.length} items; all items up-to-date`);
+			}
+
 		}
-		console.log(`Processed ${filename}`);
+		else {
+			console.log(`No items to process in ${filename}`);
+		}
 	}
 	catch (e) {
 		console.error(`Error processing ${filename}:`);
