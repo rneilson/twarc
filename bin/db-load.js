61594acfae43bc8f6de2e0c1038d5eb060dbc574
Raymond Neilson <rneilson@interloper.ca>
2016-08-28 13:40:00 -0400

    Updated db-load to use new DBWriter functions instead

--- bin/db-load.js
+++ bin/db-load.js
@@ -43,84 +43,7 @@ console.log('Finished!');
 // Funcs
 function loadfile (name) {
 	const types = ['user', 'user_tweet', 'other_tweet', 'delete', 'favorite', 'unfavorite'];
-	let count = {
-		'user': 0,
-		'user_tweet': 0,
-		'other_tweet': 0,
-		'delete': 0,
-		'favorite': 0,
-		'unfavorite': 0
-	};
 	let filename = path.resolve(name);
-	let ret = false;
-	let total = 0;
-
-	function writefn (type, data, txn) {
-		let retw = false;
-
-		switch (type) {
-			case 'queue':
-				retw = writeq(data);
-				break;
-			case 'status':
-				retw = dbw.setstatus('', data, txn);
-				break;
-			case 'following':
-				retw = dbw.setstatus('following', data, txn);
-				break;
-			case 'user':
-				retw = dbw.writeuser(data, txn);
-				break;
-			case 'user_tweet':
-				retw = dbw.writetweet(data, txn);
-				break;
-			case 'other_tweet':
-				retw = dbw.writetweet(data, txn);
-				break;
-			case 'delete':
-				retw = dbw.deletetweet(data, txn);
-				break;
-			case 'favorite':
-				retw = dbw.writefav(data, txn);
-				break;
-			case 'unfavorite':
-				retw = dbw.deletefav(data, txn);
-				break;
-		}
-
-		return retw;
-	}
-
-	function writeq (queue) {
-		let retq = false;
-
-		// Open transaction
-		let qtxn = dbw.begin();
-
-		try {
-			// Recursively call
-			for (let i = 0; i < queue.length; i++) {
-				let item = queue[i];
-				if (_.has(item, 'type') && _.has(item, 'data')) {
-					// Leave return value to end of queue, but return true if any write succeeds
-					if (writefn(item.type, item.data, qtxn)) {
-						retq = true;
-						if (_.has(count, item.type)) {
-							count[item.type]++;
-						}
-					}
-				}
-			}
-			qtxn.commit();
-		}
-		catch (e) {
-			qtxn.abort();
-			console.error(e);
-			retq = false;
-		}
-
-		return retq;
-	}
 
 	try {
 		// Read file into memory
@@ -130,22 +53,33 @@ function loadfile (name) {
 		let eventlist = JSON.parse(str);
 
 		if (eventlist.length > 0) {
-			console.log(`Processing ${filename}, ${eventlist.length} items`);
-			if (writeq(eventlist)) {
+			console.log(`Processing ${filename}, ${eventlist.length} items...`);
+			let ret = dbw.writequeue(eventlist);
+			if (ret) {
+				let total = 0;
 				let counts = [];
+				let stastr = '';
+				if (ret.status.size > 0) {
+					let num = ret.status.size;
+					total += num;
+					stastr = '\nUpdated status items:';
+					for (let st of ret.status) {
+						stastr += `\n    ${st}`;
+					}
+					counts.push(`${num} status item${num > 1 ? 's' : ''}`);
+				}
 				for (let name of types) {
-					let num = count[name];
+					let num = ret[name];
 					if (num > 0) {
 						total += num;
-						counts.push(`${num} ${name}${num > 1 ? 's' : ''}`);
+						counts.push(`${num} ${name}${num == 1 ? '' : name.endsWith('s') ? 'es' : 's'}`);
 					}
 				}
-				console.log(`Processed ${eventlist.length} items, updated ${total} (${counts.join(', ')})`);
+				console.log(`Updated ${total} items (${counts.join(', ')})` + stastr);
 			}
 			else {
-				console.log(`Processed ${eventlist.length} items; all items up-to-date`);
+				console.log(`All items up-to-date`);
 			}
-
 		}
 		else {
 			console.log(`No items to process in ${filename}`);
