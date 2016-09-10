87bc626264df3f8e6ed4363cc0729053254340db
Raymond Neilson <rneilson@interloper.ca>
2016-09-09 22:23:42 -0400

    Changed db-dump to use new cursor syntax

--- bin/db-dump.js
+++ bin/db-dump.js
@@ -4,7 +4,7 @@
 const _ = require('lodash');
 const path = require('path');
 const util = require('util');
-const lmdb = require('../node-lmdb');
+const lmdb = require('node-lmdb');
 const Filters = require('../lib/filters.js');
 const DBReader = require('../lib/dbread.js');
 
@@ -48,10 +48,11 @@ txn = dbr.begin();
 cur = new lmdb.Cursor(txn, dbr.dbs.status);
 
 acc = {};
-writer = (k, v) => _.set(acc, k, JSON.parse(v.toString()));
+// writer = (k, v) => _.set(acc, k, JSON.parse(v.toString()));
 
 for (key = cur.goToFirst(); key; key = cur.goToNext()) {
-	cur.getCurrentBinaryUnsafe(writer);
+	// cur.getCurrentBinaryUnsafe(writer);
+	_.set(acc, key, JSON.parse(cur.getCurrentBinaryUnsafe().toString()));
 }
 
 writefn('status_raw', acc);
@@ -64,10 +65,11 @@ txn.commit();
 txn = dbr.begin();
 cur = new lmdb.Cursor(txn, dbr.dbs.users);
 
-writer = (k, v) => writefn('user', JSON.parse(v.toString()));
+// writer = (k, v) => writefn('user', JSON.parse(v.toString()));
 
 for (key = cur.goToFirst(); key; key = cur.goToNext()) {
-	cur.getCurrentBinaryUnsafe(writer);
+	// cur.getCurrentBinaryUnsafe(writer);
+	writefn('user', JSON.parse(cur.getCurrentBinaryUnsafe().toString()));
 }
 
 cur.close();
@@ -78,8 +80,24 @@ txn.commit();
 txn = dbr.begin();
 cur = new lmdb.Cursor(txn, dbr.dbs.tweets);
 
-writer = (k, v) => {
-	let t = JSON.parse(v.toString());
+// writer = (k, v) => {
+// 	let t = JSON.parse(v.toString());
+// 	if (t.deleted) {
+// 		// Convert from stored format to broadcast format
+// 		writefn('delete', {
+// 			id_str: t.id_str,
+// 			user_id_str: t.user.id_str,
+// 			time: t.deleted
+// 		});
+// 	}
+// 	else {
+// 		writefn(filters.user(t.user) ? 'user_tweet' : 'other_tweet', t);
+// 	}
+// };
+
+for (key = cur.goToFirst(); key; key = cur.goToNext()) {
+	// cur.getCurrentBinaryUnsafe(writer);
+	let t = JSON.parse(cur.getCurrentBinaryUnsafe().toString());
 	if (t.deleted) {
 		// Convert from stored format to broadcast format
 		writefn('delete', {
@@ -91,10 +109,6 @@ writer = (k, v) => {
 	else {
 		writefn(filters.user(t.user) ? 'user_tweet' : 'other_tweet', t);
 	}
-};
-
-for (key = cur.goToFirst(); key; key = cur.goToNext()) {
-	cur.getCurrentBinaryUnsafe(writer);
 }
 
 cur.close();
@@ -105,10 +119,11 @@ txn.commit();
 txn = dbr.begin();
 cur = new lmdb.Cursor(txn, dbr.dbs.favids);
 
-writer = (k, v) => writefn('favorite', {id_str: k, time: parseInt(v)});
+// writer = (k, v) => writefn('favorite', {id_str: k, time: parseInt(v)});
 
 for (key = cur.goToFirst(); key; key = cur.goToNext()) {
-	cur.getCurrentString(writer);
+	// cur.getCurrentString(writer);
+	writefn('favorite', {id_str: key, time: parseInt(cur.getCurrentString())})
 }
 
 cur.close();
@@ -140,26 +155,36 @@ function writeindex (db, type) {
 	txn = dbr.begin();
 	cur = new lmdb.Cursor(txn, db);
 
-	key = cur.goToFirst();
-	if (key !== null) {
-		cur.getCurrentString((k, v) => {
+	let k = cur.goToFirst();
+	if (k !== null) {
+		// cur.getCurrentString((k, v) => {
+		// 	if (!v.startsWith('[')) {
+		// 		v = `"${v}"`;
+		// 	}
+		// 	str = prefix + `{\n  "type": "${type}",\n  "data": {\n    "${k}": ${v}`;
+		// });
+		let v = cur.getCurrentString();
+		if (!v.startsWith('[')) {
+			v = `"${v}"`;
+		}
+		let str = prefix + `{\n  "type": "${type}",\n  "data": {\n    "${k}": ${v}`;
+
+		while (k = cur.goToNext()) {
+			// cur.getCurrentString((k, v) => {
+			// 	if (!v.startsWith('[')) {
+			// 		v = `"${v}"`;
+			// 	}
+			// 	str += `,\n    "${k}": ${v}`;
+			// });
+			let v = cur.getCurrentString();
 			if (!v.startsWith('[')) {
 				v = `"${v}"`;
 			}
-			acc = prefix + `{\n  "type": "${type}",\n  "data": {\n    "${k}": ${v}`;
-		});
-
-		while (cur.goToNext()) {
-			cur.getCurrentString((k, v) => {
-				if (!v.startsWith('[')) {
-					v = `"${v}"`;
-				}
-				acc += `,\n    "${k}": ${v}`;
-			});
+			str += `,\n    "${k}": ${v}`;
 		}
 
-		acc += `\n  }\n}`;
-		process.stdout.write(acc);
+		str += `\n  }\n}`;
+		process.stdout.write(str);
 	}
 
 	cur.close();
