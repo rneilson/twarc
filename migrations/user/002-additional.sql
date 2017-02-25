a24ec956d7b93d168967a9f3d9cebf78807f72fc
Raymond Neilson <rneilson@interloper.ca>
2017-02-25 01:12:38 -0500

    Added triggers on alias, mention tables

--- migrations/user/002-additional.sql
+++ migrations/user/002-additional.sql
@@ -5,7 +5,12 @@
 -- Notes:
 --  - alias and mention are one-off insert-only
 --  - alias will store old screen names in case of older tweet text
---  - mention is basically a big index for tweets' user_mentions
+--    - timestamp indicates when user_id/screen_name combo first seen
+--    - new alias entry will be made when user inserted or screen_name updated
+--    - alias entries must be manually inserted for mentions
+--  - mention is basically a big index for tweets' user_mentions plus RTs/quotes
+--    - mention timestamps will be updated when tweet timestamps are
+--    - mentions will be deleted when tweets are (normally handled by foreign keys)
 --  - deleted will store original tweet object if available, despite
 --    technically being required to actually delete stored tweets
 
@@ -17,6 +22,21 @@ CREATE TABLE alias (
 );
 CREATE INDEX idx_alias_by_screen_name ON alias (screen_name);
 CREATE INDEX idx_alias_by_user_by_time ON alias (user_id, timestamp_ms DESC);
+CREATE TRIGGER tri_add_alias_on_user_insert
+  AFTER INSERT ON user
+  WHEN new.screen_name IS NOT NULL
+  BEGIN
+    INSERT OR REPLACE INTO alias (user_id, screen_name, timestamp_ms)
+    VALUES (new.id, new.screen_name, new.timestamp_ms);
+  END;
+CREATE TRIGGER tri_add_alias_on_user_screen_name_update
+  AFTER UPDATE OF screen_name ON user
+  WHEN new.screen_name IS NOT NULL
+   AND new.screen_name != old.screen_name
+  BEGIN
+    INSERT OR REPLACE INTO alias (user_id, screen_name, timestamp_ms)
+    VALUES (new.id, new.screen_name, new.timestamp_ms);
+  END;
 
 
 CREATE TABLE mention (
@@ -40,6 +60,19 @@ CREATE INDEX idx_mention_retweet_by_time
 CREATE INDEX idx_mention_quote_by_time
   ON mention (quoted_user_id, timestamp_ms DESC)
   WHERE quoted_user_id IS NOT NULL;
+CREATE TRIGGER tri_update_mention_on_tweet_timestamp_update
+  AFTER UPDATE OF timestamp_ms ON tweet
+  WHEN new.timestamp_ms > old.timestamp_ms
+  BEGIN
+    UPDATE mention SET timestamp_ms = new.timestamp_ms
+    WHERE tweet_id = new.id;
+  END;
+CREATE TRIGGER tri_delete_mention_on_tweet_delete
+  BEFORE DELETE ON tweet
+  BEGIN
+    DELETE FROM mention
+    WHERE tweet_id = old.id;
+  END;
 
 
 CREATE TABLE deleted (
@@ -53,12 +86,16 @@ CREATE TABLE deleted (
 --------
 
 DROP TABLE deleted;
+DROP TRIGGER tri_delete_mention_on_tweet_delete;
+DROP TRIGGER tri_update_mention_on_tweet_timestamp_update;
 DROP INDEX idx_mention_quote_by_time;
 DROP INDEX idx_mention_retweet_by_time;
 DROP INDEX idx_mention_reply_by_time;
 DROP INDEX idx_mention_of_user_by_time;
 DROP INDEX idx_mention_by_user_by_time;
 DROP TABLE mention;
+DROP TRIGGER tri_add_alias_on_user_screen_name_update;
+DROP TRIGGER tri_add_alias_on_user_insert;
 DROP INDEX idx_alias_by_user_by_time;
 DROP INDEX idx_alias_by_screen_name;
 DROP TABLE alias;
