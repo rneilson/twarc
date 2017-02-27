aeb8d6de586961284db18b32e0b57230be060001
Raymond Neilson <rneilson@interloper.ca>
2017-02-27 12:32:38 -0500

    Filled out tests for getconfig(), setconfig()

--- test/db-base.js
+++ test/db-base.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const { expect } = require('chai');
+const { decompose, recombine } = require('../lib/parsecfg');
 const BaseDB = require('../lib/db-base');
 
 describe('BaseDB', function () {
@@ -71,29 +72,189 @@ describe('BaseDB', function () {
 
   describe('getconfig()', function () {
 
-    it('should get a single value for a single key');
+    let db;
 
-    it('should get an object value for a key prefix');
+    before(function () {
+      db = new BaseDB('./test/data/db-base.db', {
+        create_file: true,
+        single_conn: true
+      });
 
-    it('should get all values as an object for an empty key');
+      return db.db.migrate({migrationsPath: './test/migrations/db-base'});
+    });
 
-    it('should return a [value, time] array when time is true');
+    it('should get a single value for a single key', function () {
+      return db.getconfig('app.test1').then((val) => {
+        expect(val).to.be.true;
+      })
+    });
+
+    it('should get an object value for a key prefix', function () {
+      return db.getconfig('dir').then((obj) => {
+        expect(obj).to.deep.equal({
+          test: './test/',
+          sql: './test/migrations/db-base/'
+        });
+      });
+    });
+
+    it('should get all values as an object for an empty key', function () {
+      return db.getconfig().then((obj) => {
+        expect(obj).to.deep.equal({
+          app: {
+            test1: true,
+            test2: false,
+            test3: null,
+            obj: {
+              test1: 'beep',
+              test2: 'boop',
+              test3: ['beep', 'boop']
+            }
+          },
+          dir: {
+            test: './test/',
+            sql: './test/migrations/db-base/'
+          }
+        });
+      });
+    });
+
+    it('should return a [value, time] array when time is true', function () {
+      return db.getconfig('app.test1', true).then((val) => {
+        expect(val).to.deep.equal([true, 1488210932000]);
+      });
+    });
+
+    after(function () {
+      return db.close();
+    });
 
   });
 
   describe('setconfig()', function () {
 
-    it('should update a single value for a single key');
+    let db;
 
-    it('should update multiple values for a prefix key');
+    before(function () {
+      db = new BaseDB('./test/data/db-base.db', {
+        create_file: true,
+        single_conn: true
+      });
 
-    it('should insert a new key for a new single value');
+      return db.db.migrate({migrationsPath: './test/migrations/db-base'});
+    });
 
-    it('should insert multiple new keys for a new object value');
+    it('should update a single value for a single key', function () {
+      return db.setconfig('./test/data/', 'dir.test').then((chg) => {
+        expect(recombine(chg, 'dir.test')).to.be.true;
+      })
+      .then(() => db.getconfig('dir.test'))
+      .then((val) => {
+        expect(val).to.equal('./test/data/');
+      });
+    });
 
-    it('should not update a value when the existing time is later');
+    it('should update multiple values for a prefix key', function () {
+      return db.setconfig(
+        {
+          test1: 'boop',
+          test2: 'beep',
+          test3: ['boop', 'beep']
+        },
+        'app.obj'
+      )
+      .then((chg) => {
+        expect(recombine(chg, 'app.obj')).to.deep.equal({
+          test1: true,
+          test2: true,
+          test3: true
+        });
+      })
+      .then(() => db.getconfig('app.obj'))
+      .then((val) => {
+        expect(val).to.deep.equal({
+          test1: 'boop',
+          test2: 'beep',
+          test3: ['boop', 'beep']
+        });
+      });
+    });
 
-    it('should not insert a new key conflicting with an existing prefix');
+    it('should insert a new key for a new single value', function () {
+      return db.getconfig('app.test4').then((val) => {
+        expect(val).to.be.undefined;
+      })
+      .then(() => db.setconfig(10, 'app.test4'))
+      .then((chg) => {
+        expect(recombine(chg, 'app.test4')).to.be.true;
+      })
+      .then(() => db.getconfig('app.test4'))
+      .then((val) => {
+        expect(val).to.equal(10);
+      });
+    });
+
+    it('should insert multiple new keys for a new object value', function () {
+      return db.getconfig('test').then((val) => {
+        expect(val).to.be.undefined;
+      })
+      .then(() => db.setconfig(
+        {
+          beep: 2,
+          boop: '3'
+        },
+        'test'
+      ))
+      .then((chg) => {
+        expect(recombine(chg, 'test')).to.deep.equal({
+          beep: true,
+          boop: true
+        });
+      })
+      .then(() => db.getconfig('test'))
+      .then((val) => {
+        expect(val).to.deep.equal({
+          beep: 2,
+          boop: '3'
+        });
+      });
+    });
+
+    it('should not update a value when the existing time is later', function () {
+      return db.setconfig(1, 'app.test1', 1488210931999).then((chg) => {
+        expect(recombine(chg, 'app.test1')).to.be.false;
+      })
+      .then(() => db.getconfig('app.test1', true))
+      .then((val) => {
+        expect(val).to.deep.equal([true, 1488210932000]);
+      })
+      .then(() => db.setconfig([1, 1488210931999], 'app.test1', true))
+      .then((chg) => {
+        expect(recombine(chg, 'app.test1')).to.be.false;
+      })
+      .then(() => db.getconfig('app.test1', true))
+      .then((val) => {
+        expect(val).to.deep.equal([true, 1488210932000]);
+      });
+    });
+
+    it('should not insert a key conflicting with an existing prefix', function () {
+      return db.setconfig(5, 'test').catch((e) => {
+        expect(e).to.be.an('error');
+        expect(e.message).to.match(/already a prefix/i);
+      })
+      .then(() => db.getconfig('test'))
+      .then((val) => {
+        expect(val).to.deep.equal({
+          beep: 2,
+          boop: '3'
+        });
+      });
+    });
+
+    after(function () {
+      return db.close();
+    });
 
   });
 
