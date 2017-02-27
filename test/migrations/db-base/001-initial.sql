166dad33728adbbaed95cd64c3f9b26256d76551
Raymond Neilson <rneilson@interloper.ca>
2017-02-27 12:32:11 -0500

    Added explicit times to db-base migration test values

--- test/migrations/db-base/001-initial.sql
+++ test/migrations/db-base/001-initial.sql
@@ -7,15 +7,15 @@ CREATE TABLE config (
   value TEXT,
   time_ms INTEGER
 );
-INSERT INTO config (key, value) VALUES
-  ('app.test1', 'true'),
-  ('app.test2', 'false'),
-  ('app.test3', 'null'),
-  ('app.obj.test1', '"beep"'),
-  ('app.obj.test2', '"boop"'),
-  ('app.obj.test3', '["beep", "boop"]'),
-  ('dir.test', '"./test/"'),
-  ('dir.sql', '"./test/migrations/db-base/"');
+INSERT INTO config (key, value, time_ms) VALUES
+  ('app.test1', 'true', 1488210932000),
+  ('app.test2', 'false', 1488210932000),
+  ('app.test3', 'null', 1488210932000),
+  ('app.obj.test1', '"beep"', 1488210932000),
+  ('app.obj.test2', '"boop"', 1488210932000),
+  ('app.obj.test3', '["beep", "boop"]', 1488210932000),
+  ('dir.test', '"./test/"', 1488210932000),
+  ('dir.sql', '"./test/migrations/db-base/"', 1488210932000);
 
 
 --------
