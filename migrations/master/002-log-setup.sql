13fab0d7bb2a1232cb49db77483a41ff88271826
Raymond Neilson <rneilson@interloper.ca>
2017-03-02 11:01:35 -0500

    'event' is a little too generic as a log type, changed to 'notify'

--- migrations/master/002-log-setup.sql
+++ migrations/master/002-log-setup.sql
@@ -13,7 +13,7 @@ CREATE TABLE log_type (
 INSERT INTO log_type (code, label, description, to_db, to_file, to_console) VALUES
   (0, 'error', 'Error', 1, 1, 1),
   (1, 'warning', 'Warning', 0, 1, 1),
-  (2, 'event', 'Event', 1, 1, 1),
+  (2, 'notify', 'Notification', 1, 1, 1),
   (3, 'info', 'Information', 0, 1, 1),
   (4, 'display', 'Display', 0, 0, 1),
   (5, 'debug', 'Debug', 0, 0, 0);
